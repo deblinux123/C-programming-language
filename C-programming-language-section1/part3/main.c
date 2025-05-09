ab36e57b94fdcc2cae4adb87881fe0cf9a1eddd7
farid <deltaburs@gmail.com>
2025-05-09 16:30:36 +0330

    Adding  part3 of the section 1

---
+++ C-programming-language-section1/part3/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+    int total;
+    // compiler warnings
+    printf("This code below was runing but with warning that not valid number in total variable\n");
+	printf("This is an compiler warnings %d\n", total);
+    
+    prinf("And this one gives os error from complier\n");
+    printf(age); // age is not initiolize
+	return 0;
+}
