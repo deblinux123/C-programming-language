4859f6a989514a9ba0e8bb944c500ec048041de7
farid <deltaburs@gmail.com>
2025-05-09 16:43:27 +0330

    Adding the part 4 and linker error

---
+++ C-programming-language-section1/part4/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+
+
+void printMessage(); // Decloration only
+
+int main(int argc, char **argv)
+{
+	printf("Linker error in C");
+    printMessage(); // dont have eny error from compile the project
+	return 0;
+}
