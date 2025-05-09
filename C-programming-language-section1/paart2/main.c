345792d74cdbe62caee96916498adb6e718c53a6
farid <deltaburs@gmail.com>
2025-05-09 15:54:09 +0330

    Updating my workspace and adding the part 2 of the section 1

---
+++ C-programming-language-section1/paart2/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int age = 0;
+    
+    printf("How old are you please enter your age: \n");
+    
+    scanf("%d", &age);
+    
+    printf("Ow you are %d years old.", age);
+    
+    return 0;
+}
+
