4c731d71a4449165fe7694dde9ee3eed2888824a
debra <deltaburs@gmail.com>
2025-05-21 14:06:20 +0330

    adding the challeng for this sections

--- C-programming-language-section8/challeng1/main.c
+++ C-programming-language-section8/challeng1/main.c
@@ -1,7 +1,57 @@
 #include <stdio.h>
+#include <string.h>
 
-int main(int argc, char **argv)
+
+
+
+void reverseString(char txt[]);
+void bubbleSort(int arr[], int size);
+
+int main()
 {
-	printf("hello world\n");
-	return 0;
+    char text[] = "Hello my name is ali";
+    reverseString(text);
+    
+    int num[] = {12, 3, 4, 2, 41, 43};
+    int size = sizeof(num)/ sizeof(num[0]);
+    
+    bubbleSort(num, size);
+    
+    return 0;
+}
+
+
+void reverseString(char txt[])
+{
+    int len = strlen(txt);
+    printf("Revers is:\n");
+    
+    for (int i = 0; i <= len; i++){
+        printf("%c", txt[len - i]);
+    }
+    
+    printf("\n\n");
+}
+
+void bubbleSort(int arr[], int size)
+{
+    int temp;
+    
+    for (int i = 0; i < size - 1; i++){
+        for (int j = 0; j < size - i - 1; j++)
+        {
+            if(arr[j] > arr[j + 1])
+            {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    
+    printf("Sorted Array: ");
+    for (int k=0; k < size; k++)
+        printf("%d\t", arr[k]);
+        
+    printf("\n");
 }
