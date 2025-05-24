cc6f2b667e6620882ec2fd52b6d79ea53ceb1702
debra <deltaburs@gmail.com>
2025-05-24 12:32:19 +0330

    Adding the part 5 of the section9

---
+++ C-programming-language-section9/part5/main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+
+// this is pass by value function
+int add(int x, int y)
+{
+    int result = x + y;
+    
+    return result;
+}
+
+// pass by reference
+int multy(int *x, int *y)
+{
+    int result = *x * *y;
+    return result;
+}
+
+
+int main(int argc, char **argv)
+{
+    int a=10;
+    int b=10;
+
+    int addTwoNumber = 0;
+    addTwoNumber = add(a, b);
+    printf("%d + %d = %d\n", a, b, addTwoNumber);
+    
+    int multiplyTwoNumber = 0;
+    multiplyTwoNumber = multy(&a, &b);
+    printf("This is the result for %d * %d = %d\n", a, b, multiplyTwoNumber);
+    
+	return 0;
+}
