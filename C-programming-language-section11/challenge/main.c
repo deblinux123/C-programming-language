612374e4bd15d1157a20b184d61ef7a9e93554a8
debra <deltaburs@gmail.com>
2025-05-27 12:35:39 +0330

    Struct challlenge

---
+++ C-programming-language-section11/challenge/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+    struct employee {
+        char name[50];
+        int hireDate;
+        float salary;
+    };
+    
+    struct employee employ1 = {"jack", 12, 123.900};
+    
+    printf("Ok %s you hire ate day %i and your salary is %.2f\n", employ1.name, employ1.hireDate, employ1.salary);
+    
+    struct employee newEmploy;
+    
+    printf("Enter your name: \n");
+    scanf("%s", newEmploy.name);
+    
+    printf("Enter your hire date: \n");
+    scanf("%d", &newEmploy.hireDate);
+    
+    printf("How mach slsary do you get: \n");
+    scanf(" %f", &newEmploy.salary);
+    
+    printf("Ok %s you hire ate day %i and your salary is %.2f\n", newEmploy.name, newEmploy.hireDate, newEmploy.salary);
+    
+    return 0;
+}
