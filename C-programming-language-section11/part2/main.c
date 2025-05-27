70606ec7718440da3a97a1a9daa2de57e8c09a1e
debra <deltaburs@gmail.com>
2025-05-27 13:57:15 +0330

    Modifying

--- C-programming-language-section11/part2/main.c
+++ C-programming-language-section11/part2/main.c
@@ -1,34 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
 
- struct employee {
-        char name[50];
-        int hireDate;
-        float salary;
-    };
+
+// this note allocate memory
+struct date 
+{
+    int month;
+    int day;
+    int year;
+};
+
+
+struct intPtr
+{
+    int *p1;
+    int *p2;
+};
 
 
 int main()
 {
-    struct date 
-    {
-        int month;
-        int day;
-        int year;
-    };
-    
+
     struct date today, *datePtr;
-    
-    
+
     datePtr = &today;
     
     datePtr -> month = 9;
     datePtr -> day = 23;
     datePtr -> year = 1990;
-    
-    
+
     printf("Today's Date: \n");
     printf("%i/%i/%i\n", datePtr -> month, datePtr -> day, datePtr -> year);
+    
+    
+    printf("\n\n==============================================================\n\n");
+    
+    struct intPtr pointers;
+    int i1 = 100, i2;
+    pointers.p1 = &i1;
+    pointers.p2 = &i2;
+    *pointers.p2 = -98;
+    
+    printf("i1 = %i, *pointers.p1 = %i\n", i1, *pointers.p1);
+    printf("i2 = %i, *pointers.p2 = %i\n", i2, *pointers.p2);
     
     return 0;
 }
