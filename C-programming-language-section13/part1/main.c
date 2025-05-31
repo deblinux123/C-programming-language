a7b3bb0192fd067503d97a093842d92b649fefb1
debra <deltaburs@gmail.com>
2025-05-31 13:25:54 +0330

    Final section and all about standard library in C

---
+++ C-programming-language-section13/part1/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/*========================================================
+ *                   C standard library for C|EH
+ * 
+ * 
+ * 1. <string.h>: String Manipulation & Exploits
+ *
+ * 2. <stdio.h>: Input/Output & File Attacks
+ * 
+ * 3. <stdlib.h>: Memory/Process Control
+ * 
+ * 4. <netdb.h> & <sys/socket.h> (POSIX): Network Exploitation
+ * 
+ * 5. <ctype.h> & <errno.h>: Defense & Analysis
+ * 
+ */
+
+
+int main(int argc, char **argv)
+{
+	printf("Study Recommendations\n");
+	return 0;
+}
